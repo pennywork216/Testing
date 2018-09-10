43c0ff6cf6dda785708bb7d44caa5c418c04c10a
pennywork216 <43124291+pennywork216@users.noreply.github.com>
2018-09-10 10:59:19 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] test = {1, 2, 3};
+        int b = 0;
+        for (int i = 0; i <= test.length; i++) {
+            int a = test[i];
+        }
+        if (true || false) {
+        boolean here = true;
+        } else {boolean here = true;}
+    System.out.println("Hello WOrld");
+  }
+}
